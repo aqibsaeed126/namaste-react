86e2c8212e774a36172cd62d70d69b99677390fe
aqibsaeed126 <saeedaqib295@gmail.com>
2023-08-08 19:29:46 +0500

    Episode 01 - Practice Files

---
+++ Ep-1-Inception/App.js
@@ -0,0 +1,19 @@
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading", abc: "xyz" },
+  "Hello World from React! I am H1 Tag"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  { id: "heading2", abc: "xyz" },
+  "Small Hello World from React! I am H1 Tag"
+);
+const child = React.createElement("div", { id: "child" }, [heading1, heading2]);
+const parent = React.createElement("div", { id: "parent" }, child);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
